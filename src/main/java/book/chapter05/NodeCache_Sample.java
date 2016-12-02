42e456e7e5f8cfee79071a6e83110db3a595cef9
martin820303 <martin820303@aliyun.com>
2016-12-02 17:02:49 +0800

    新

---
+++ src/main/java/book/chapter05/NodeCache_Sample.java
@@ -0,0 +1,60 @@
+package book.chapter05;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+public class NodeCache_Sample {
+	static String path = "/zk-book";
+	static CuratorFramework client = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181").sessionTimeoutMs(5000).retryPolicy(new ExponentialBackoffRetry(100, 3)).build();
+	
+	public static void main(String[] args) throws Exception{
+		client.start();
+		client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path, "init".getBytes());
+		final NodeCache cache = new NodeCache(client, path,false);
+		cache.start(true);
+		cache.getListenable().addListener(new NodeCacheListener() {
+			
+			public void nodeChanged() throws Exception {
+				System.out.println("Node data update, new data: "+new String(cache.getCurrentData().getData()));
+			}
+		});
+		client.setData().forPath(path, "u".getBytes());
+		Thread.sleep(1000);
+		client.setData().forPath(path, "u2".getBytes());
+		Thread.sleep(10000);
+		client.delete().deletingChildrenIfNeeded().forPath(path);
+		Thread.sleep(Integer.MAX_VALUE);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
